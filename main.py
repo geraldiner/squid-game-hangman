0c6c0235461c772a475cffbd5071b56204d1d643
Geraldine <hello@geraldiner.com>
2021-12-14 03:37:27 +0000

    convert from console input to turtle.onclick

--- main.py
+++ main.py
@@ -5,108 +5,111 @@ import string
 import math
 
 #-----game configuration----
-secret = "SQUIDWARD SMELLS GOOD"
+secret = "THE COMPUTER ONLY DOES WHAT YOU TELL IT TO DO"
 unique_letters = list(set([x for x in list(secret) if x != " "]))
 wrong_letters = [
     x for x in list(string.ascii_uppercase) if x not in unique_letters
 ]
 guessed_so_far = ""
 guessed_letters = []
-guesses_left = math.floor(len(unique_letters)/2)-1
+guesses_left = math.floor(len(unique_letters) / 2) - 1
 game_over = False
 game_won = False
 
 font_size = 12
 font_family = "Arial"
 
-image = "startbutton.gif"
-
-start_x = -200
-panel_size = 40
+panel_shapesize = 5
+panel_size = panel_shapesize * 5
+start_x = -panel_size * 2.5 - 10
 total_panels = len(unique_letters)
 current_panel_num = 0
 current_correct_letter = ""
 current_wrong_letter = ""
+panel1_letter = ""
+panel2_letter = ""
 
 #-----initialize turtle-----
 
 ### Screen Turtle
 wn = turtle.Screen()
 wn.setup(width=1.0, height=1.0)
-wn.addshape(image)
-
-### Start Button Turtle
-start = turtle.Turtle(image)
+wn.addshape("startbutton.gif")
+wn.bgpic("squidgame.png")
 
 ### Feedback Writer Turtle
 writer = turtle.Turtle("blank")
 writer.hideturtle()
+writer.color("yellow")
 writer.penup()
 writer.home()
 writer.sety(150)
 
 ### Panel 1
-panel1 = turtle.Turtle()
+panel1 = turtle.Turtle("square")
 panel1.hideturtle()
+panel1.color("gray")
+panel1.shapesize(panel_shapesize)
+panel1.penup()
 panel1.speed(0)
 
 ### Panel 1 Writer
 panel1_writer = turtle.Turtle("blank")
 panel1_writer.hideturtle()
+panel1_writer.color("white")
 
 ### Panel 2
-panel2 = turtle.Turtle()
+panel2 = turtle.Turtle("square")
 panel2.hideturtle()
+panel2.color("gray")
+panel2.shapesize(panel_shapesize)
+panel2.penup()
 panel2.speed(0)
 
 ### Panel 2 Writer
 panel2_writer = turtle.Turtle("blank")
 panel2_writer.hideturtle()
+panel2_writer.color("white")
 
 ## Player
 player = turtle.Turtle("arrow")
 player.hideturtle()
+player.color("green")
 player.penup()
 player.goto(start_x - panel_size, 0)
+
+### Start Button Turtle
+start = turtle.Turtle("startbutton.gif")
 
 
 #-----game functions--------
 def run_game(x, y):
-	global guesses_left, game_over
 	start.hideturtle()
 	player.showturtle()
-	while game_over == False:
+	panel1.showturtle()
+	panel2.showturtle()
+	reveal_letters()
+	play_game()
+
+
+
+def play_game():
+	global game_over
+	if (not game_over):
 		if (guesses_left > 0):
-			reveal_letters()
-			write_to_screen(
-			    guessed_so_far +
-			    "\n\nEnter your choice in the console and press ENTER.\n\nYou have "
-			    + str(guesses_left) + " guess(es) left.")
-			draw_panel_pairs(start_x + (panel_size * current_panel_num),
-			                 panel_size)
-			write_panel_letters(start_x + (panel_size * current_panel_num),
-			                    panel_size)
-			user_guess = input("Type a letter and press ENTER. ").upper()
-			if (user_guess == ""):
-				print("You just gave up a guess. -1 guess\n")
-				guesses_left -= 1
-			elif (not user_guess.isalpha()):
-				print("Your guess must be a letter. -1 guess\n")
-				guesses_left -= 1
-			elif (user_guess != current_correct_letter
-			      and user_guess != current_wrong_letter):
-				print("You must choose between the given letters. -1 guess\n")
-				guesses_left -= 1
-			else:
-				check_guess(user_guess)
+			write_to_screen(guessed_so_far + "\n\nChoose the next panel wisely.\n(Make sure to click on the panel, not the letter.)\n\nYou have " + str(guesses_left) + " guess(es) left.")
+			move_panel_pairs(start_x, 0)
+			write_panel_letters(start_x, 0)
 		else:
 			game_over = True
-			write_to_screen(guessed_so_far + "\n\nToo bad so sad, you lost.")
+	else:
+		display_results()
+
+
+def display_results():
 	if (game_won == False):
 		write_to_screen(guessed_so_far + "\n\nToo bad so sad, you lost.")
 	else:
-		player.goto(
-		    start_x + (panel_size / 2) + (panel_size * current_panel_num), 0)
 		write_to_screen(guessed_so_far +
 		                "\n\nCongratulations, you made it across alive.")
 
@@ -125,34 +128,37 @@ def reveal_letters():
 
 def write_to_screen(phrase):
 	writer.clear()
-	writer.write(phrase,
-	             align="center",
-	             font=(font_family, font_size, "normal"))
+	writer.write(phrase, align="center", font=(font_family, font_size, "bold"))
+
+
+def move_panel_pairs(x, y):
+	panel1.goto(x, y)
+	panel2.goto(x + panel_size * 4 + 10, y)
 
 
 def draw_panel_pairs(x, y):
 	draw_panel(panel1, x, y)
 	draw_panel(panel2, x, y - panel_size)
-	player.goto(start_x + (panel_size * current_panel_num), 0)
+	player.goto(start_x, 0)
 
 
 def write_panel_letters(x, y):
-	global current_correct_letter, current_wrong_letter
+	global current_correct_letter, current_wrong_letter, panel1_letter, panel2_letter
 	current_correct_letter = get_letter(unique_letters)
 	current_wrong_letter = get_letter(wrong_letters)
-	heads = random.randint(0,1)
+	heads = random.randint(0, 1)
 	if heads == 1:
-		write_panel_letter(panel1_writer, x + (panel_size / 2),
-		                   y - (panel_size / 2), current_correct_letter)
-		write_panel_letter(panel2_writer, x + (panel_size / 2),
-		                   y - panel_size - (panel_size / 2),
-		                   current_wrong_letter)
+		panel1_letter = current_correct_letter
+		panel2_letter = current_wrong_letter
+		print(panel1_letter, panel2_letter)
+		write_panel_letter(panel1_writer, x, y, current_correct_letter)
+		write_panel_letter(panel2_writer, x + panel_size * 4 + 10, y,current_wrong_letter)
 	else:
-		write_panel_letter(panel1_writer, x + (panel_size / 2),
-		                   y - (panel_size / 2), current_wrong_letter)
-		write_panel_letter(panel2_writer, x + (panel_size / 2),
-		                   y - panel_size - (panel_size / 2),
-		                   current_correct_letter)
+		print(panel1_letter, panel2_letter)
+		panel1_letter = current_wrong_letter
+		panel2_letter = current_correct_letter
+		write_panel_letter(panel1_writer, x + panel_size * 4 + 10, y,current_wrong_letter)
+		write_panel_letter(panel2_writer, x, y, current_correct_letter)
 
 
 def draw_panel(panel, x, y):
@@ -172,9 +178,7 @@ def write_panel_letter(panel_writer, x, y, letter):
 	panel_writer.penup()
 	panel_writer.goto(x, y)
 	panel_writer.pendown()
-	panel_writer.write(letter,
-	                   align="center",
-	                   font=(font_family, font_size, "normal"))
+	panel_writer.write(letter,align="center",font=(font_family, panel_shapesize * 5, "bold"))
 
 
 def get_letter(choices):
@@ -184,18 +188,24 @@ def get_letter(choices):
 	return letter
 
 
+def panel1_click(x, y):
+	check_guess(panel1_letter)
+
+
+def panel2_click(x, y):
+	check_guess(panel2_letter)
+
+
 def check_guess(guess):
-	global guesses_left, guessed_letters, current_panel_num
+	global guesses_left, guessed_letters
 	guessed_letters.append(guess)
 	reveal_letters()
-	check_solved()
-
 	if guess in secret:
-		print("YOU MAY PROCEED.\n")
-		current_panel_num += 1
+		check_solved()
 	else:
-		print("TOO BAD, SO SAD. TRY AGAIN.\n")
 		guesses_left -= 1
+
+	play_game()
 
 
 def check_solved():
@@ -203,7 +213,6 @@ def check_solved():
 	if secret == guessed_so_far:
 		game_over = True
 		game_won = True
-		return
 
 
 #-----events----------------
@@ -213,6 +222,8 @@ write_to_screen(
 )
 
 start.onclick(run_game)
+panel1.onclick(panel1_click)
+panel2.onclick(panel2_click)
 
 wn.listen()
 wn.mainloop()
